d50840bf0fd33cfc345e4903d395f038fb20718e
Liuminosity <liu.stanford@gmail.com>
2015-10-29 12:44:37 -0700

    [setup] (client): set up gulp environment

---
+++ client/src/app.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var App = React.createClass({
+
+  getInitialState: function getInitialState() {
+    return {
+    }
+  },
+
+  render: function render() {
+    return (<div> Sup world </div>)
+  },
+})
