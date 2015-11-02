ef7685d1330a5cd2e1f41e6faaf2194c4b683d17
Liuminosity <liu.stanford@gmail.com>
2015-11-01 17:05:07 -0800

    [deployment] (general): add auth files

---
+++ instagramAuth.js
@@ -0,0 +1,5 @@
+module.exports = {
+  clientID : '28ab5846e177432cb064dee774cd29c3',
+  clientSecret: '3ccda79efc1b438ea536e70629325136',
+  redirectURL: 'https://boiling-headland-4189.herokuapp.com/'
+};
