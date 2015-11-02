419cf463f0d3c0948322257d4014d7aea6b8307a
Liuminosity <liu.stanford@gmail.com>
2015-11-01 21:52:21 -0800

    [bug] (server): change /db test route

--- server/server.js
+++ server/server.js
@@ -5,8 +5,6 @@ var pg = require('pg');
 
 var app = express();
 
-app.set('view engine', 'ejs');
-
 app.use(express.static(__dirname + '/../client'));
 
 app.use(bodyParser.json());
@@ -24,10 +22,12 @@ app.get('/db', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
     client.query('SELECT * FROM test_table', function(err, result) {
       done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.render('pages/db', {results: result.rows} ); }
+      if (err) { 
+        console.error(err); response.send("Error " + err); 
+      } else { 
+        console.log(result)
+        response.send(result); 
+      }
     });
   });
 })
